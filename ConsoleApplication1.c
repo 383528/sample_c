ec63e4937c9e8c6ffc6979778c9e21066f8a4d67
383528 <tu2.25.11.30@gmail.com>
2021-03-07 13:58:53 +0900

    値渡し、参照渡しの練習

--- ConsoleApplication1.c
+++ ConsoleApplication1.c
@@ -1,9 +1,28 @@
 ﻿#include <stdio.h>
 #include <math.h>
 
+int add(int a, int b) {
+    return a + b;
+}
+void p_add(int *ans, int a, int b) {
+    *ans = a + b;
+    printf("%d\n", *ans);
+}
 int main()
 {
-    int result = pow(3,7);
-    printf("%d\n",result);
+    int a = 40;
+    int b = 30;
+    int ans=0;
+
+    printf("%d\n", ans);
+
+    ans = add(a, b);
+
+    printf("%d\n", ans);
+
+    p_add(&ans, a, b);
+
+    return 0;
+
 
 }
